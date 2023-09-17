de36098306fdcaea74447b3d05bdd848c7ad431d
Suleiman Odetoro <odetoro75@gmail.com>
2023-09-17 23:43:00 +0100

    Secure routes excpet homepage with authentication

---
+++ middleware.ts
@@ -0,0 +1,16 @@
+// define code that runs before every route e.h / or /home etc
+
+import { authMiddleware } from "@clerk/nextjs";
+ 
+// This example protects all routes including api/trpc routes
+// Please edit this to allow other routes to be public as needed.
+// See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your middleware
+export default authMiddleware({
+    // remove protection from / route (homepage)
+    publicRoutes: ["/"]
+});
+ 
+export const config = {
+      matcher: ['/((?!.+\\.[\\w]+$|_next).*)', '/', '/(api|trpc)(.*)'],
+};
+ 
